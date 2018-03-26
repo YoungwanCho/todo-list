78ca384e4bff4b5f2de2bfe316e269a15c386f4e
developer119 <developer119korea@gmail.com>
2018-03-26 20:03:16 +0900

    사용하지 않을 것들 제거

--- src/App.js
+++ src/App.js
@@ -14,53 +14,53 @@ class App extends Component {
     ]
   }
 
-  handleChange = (e) => {
-    this.setState({
-      input: e.target.value
-    });
-  }
+  // handleChange = (e) => {
+  //   this.setState({
+  //     input: e.target.value
+  //   });
+  // }
 
-  handleCreate = () => {
-    const { input, todos } = this.state;
-    this.setState({
-      input: '',
-      todos: todos.concat({
-        id: this.id++,
-        text: input,
-        checked: false
-      })
-    });
-  }
+  // handleCreate = () => {
+  //   const { input, todos } = this.state;
+  //   this.setState({
+  //     input: '',
+  //     todos: todos.concat({
+  //       id: this.id++,
+  //       text: input,
+  //       checked: false
+  //     })
+  //   });
+  // }
 
-  handleKeyPress = (e) => {
-    if (e.key === 'Enter') {
-      this.handleCreate();
-    }
-  }
+  // handleKeyPress = (e) => {
+  //   if (e.key === 'Enter') {
+  //     this.handleCreate();
+  //   }
+  // }
 
-  handleToggle = (id) => {
-    const { todos } = this.state;
+  // handleToggle = (id) => {
+  //   const { todos } = this.state;
 
-    const index = todos.findIndex(todo => todo.id === id);
-    const selected = todos[index];
-    const nextTodos = [...todos];
+  //   const index = todos.findIndex(todo => todo.id === id);
+  //   const selected = todos[index];
+  //   const nextTodos = [...todos];
 
-    nextTodos[index] = {
-      ...selected,
-      checked: !selected.checked
-    };
+  //   nextTodos[index] = {
+  //     ...selected,
+  //     checked: !selected.checked
+  //   };
 
-    this.setState({
-      todos: nextTodos
-    });
-  }
+  //   this.setState({
+  //     todos: nextTodos
+  //   });
+  // }
 
-  handleRemove = (id) => {
-    const {todos} = this.state;
-    this.setState({
-      todos: todos.filter(todo =>todo.id !== id)
-    });
-  }
+  // handleRemove = (id) => {
+  //   const {todos} = this.state;
+  //   this.setState({
+  //     todos: todos.filter(todo =>todo.id !== id)
+  //   });
+  // }
 
   render() {
     const { input, todos } = this.state;
